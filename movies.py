aefca08d80bec3359821756110982d3952eac673
Krzysztof Konkol <krzysztof.konkol@jarban.com>
2019-11-01 11:38:33 +0100

    Added json_to_movie

--- movies.py
+++ movies.py
@@ -1,6 +1,71 @@
 import requests
+import json
+import re
 
+
+def get_apikey():
+    with open('apikey.json', 'r') as f:
+        apikey = json.load(f)
+    return apikey
 
 def get_movie(url, params):
     res = requests.get(url, params)
     return res.json()
+
+def json_to_movie(data):
+    awards = parse_awards(data['Awards'])
+    return Movie(
+        title=data['Title'], 
+        year=data['Year'],
+        runtime=data['Runtime'],
+        director=data['Director'],
+        language=data['Language'],
+        actors=data['Actors'],
+        oscars_won=awards['oscars_won'],
+        oscar_nominations=awards['oscar_nominations'],
+        another_wins=awards['another_wins'],
+        another_nominations=awards['another_nominations'],
+        boxoffice=data['BoxOffice'],
+        imdbRating=data['imdbRating'],
+        imdbID=data['imdbID']
+        )
+
+def parse_awards(data):
+    m1 = re.search(r'\b(\w*Won\w*)\b\s(\d*)\s\b(\w*Oscar*\w*)\b', data)
+    m2 = re.search(r'\b(\w*Nominated for\w*)\b\s(\d*)\s\b(\w*Oscar*\w*)\b', data)
+    m3 = re.search(r'(\d*)\s\b(\w*wins\w*)\b\s.\s(\d*)\s\b(\w*nomination*\w*)\b', data)
+    oscars_won = m1.group(2) if m1 else 'N/A'
+    oscar_nominations = m2.group(2) if m2 else 'N/A'
+    another_wins = m3.group(1) if m3 else 'N/A'
+    another_nominations = m3.group(3) if m3 else 'N/A'
+    awards = {
+        "oscars_won":oscars_won,
+        "oscar_nominations":oscar_nominations,
+        "another_wins":another_wins,
+        "another_nominations":another_nominations
+    }
+    return awards
+
+
+class Movie:
+    """ Movie custom class """
+
+    def __init__(self, title, year='N/A', runtime='N/A', director='N/A', language='N/A', actors='N/A', oscar_nominations='N/A', oscars_won='N/A',
+    another_wins='N/A', another_nominations='N/A', boxoffice='N/A', imdbRating='N/A', imdbID='N/A'):
+        self.title = title
+        self.year = year
+        self.runtime = runtime
+        self.director = director
+        self.language = language
+        self.actors = actors
+        self.oscars_won = oscars_won
+        self.oscar_nominations = oscar_nominations
+        self.another_wins = another_wins
+        self.another_nominations = another_nominations
+        self.boxoffice = boxoffice
+        self.imdbRating = imdbRating
+        self.imdbID = imdbID
+
+    def __str__(self):
+        return self.title
+
--- test_movies.py
+++ test_movies.py
@@ -51,5 +51,31 @@ def test_get_movie(mock_response):
     """Test that function returns JSON if both API key and movie title are provided"""
     params={"t":"Alien","apikey":auth}
     result = movies.get_movie(FAKE_URL, params=params)
-    assert 'Actors' in result
+    assert 'Alien' in result['Title']
 
+def test_get_apikey(auth):
+    """Test that function returns apikey"""
+    assert movies.get_apikey() == auth 
+
+def test_parse_awards_no_oscar():
+    data = "3 wins & 1 nomination."
+    assert movies.parse_awards(data) == {"oscars_won":'N/A', "oscar_nominations":'N/A', "another_wins":"3", "another_nominations":"1"}
+
+def test_parse_awards_oscar_nominated():
+    data = "Nominated for 7 Oscars. Another 19 wins & 32 nominations."
+    assert movies.parse_awards(data) == {"oscars_won":'N/A', "oscar_nominations":'7', "another_wins":'19', "another_nominations":'32'}
+
+def test_parse_awards_oscar_won():
+    data = "Won 1 Oscar. Another 16 wins & 19 nominations."
+    assert movies.parse_awards(data) == {"oscars_won":'1', "oscar_nominations":'N/A', "another_wins":'16', "another_nominations":'19'}
+
+def test_parse_json_to_movie(monkeypatch):
+    response = MockResponse_OK().json()
+    alien = movies.json_to_movie(response)
+    assert alien.title == 'Alien'
+    assert alien.director == 'Ridley Scott'
+    assert alien.actors == 'Tom Skerritt, Sigourney Weaver, Veronica Cartwright, Harry Dean Stanton'
+    assert alien.oscars_won == '1'
+    assert alien.oscar_nominations == 'N/A'
+    assert alien.another_wins == '16'
+    assert alien.another_nominations == '19'
